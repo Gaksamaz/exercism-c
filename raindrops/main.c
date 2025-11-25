32ee119db04ec080bfe304c7c540e1e43dd4ac39
Gaksamaz <gaksamaz@gmail.com>
2025-11-25 17:05:38 +0300

    Initial Exercism C Solution

---
+++ raindrops/main.c
@@ -0,0 +1,15 @@
+#include "raindrops.h"
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    char result[50];
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    convert(result, number);
+    printf("%s\n", result);
+
+    return 0;
+}
